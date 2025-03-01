4f0ac2e841adb01d4df981d2b154cda3d9c631f0
Vox-Net <voxnet2025@gmail.com>
2025-03-01 15:36:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -12,31 +12,30 @@
  */
 
 const fs = require("fs");
-require("dotenv").config({ path: "./config.env" });
 
-function convertToBool(text, fault = "true") {
-    return text === fault ? true : false;
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter-your-session",
-    AUTO_READ_STATUS: convertToBool(process.env.AUTO_READ_STATUS),
-    MODE: process.env.MODE || "private",
-    AUTO_VOICE: convertToBool(process.env.AUTO_VOICE),
-    AUTO_STICKER: convertToBool(process.env.AUTO_STICKER),
-    AUTO_REPLY: convertToBool(process.env.AUTO_REPLY),
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/1xb3ax.jpg",
-    ALIVE_MSG: process.env.ALIVE_MSG || "🚀 *Hello, I’m VOXNET🥷!* I’m online and ready to assist! ♻️",
-    ANTI_LINK: convertToBool(process.env.ANTI_LINK),
-    ANTI_BAD: convertToBool(process.env.ANTI_BAD),
-    PREFIX: process.env.PREFIX || ".",
-    FAKE_RECORDING: convertToBool(process.env.FAKE_RECORDING),
-    AUTO_REACT: convertToBool(process.env.AUTO_REACT),
-    HEART_REACT: convertToBool(process.env.HEART_REACT),
-    OWNER_REACT: convertToBool(process.env.OWNER_REACT),
-    BOT_NAME: process.env.BOT_NAME || "VOXNET🥷",
-    OWNER_NAME: process.env.OWNER_NAME || "KANAMBO",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "254114148625",
-    FOOTER: process.env.FOOTER || "⭐ Powered by: @VOXNET.INC",
+const session = process.env.SESSION_DATA || '';
+const prefix = process.env.PREFIX || '.';
+const mycode = process.env.CODE || "254";
+const author = process.env.STICKER_AUTHOR || 'fortunatus';
+const packname = process.env.PACKNAME || 'dreaded md2 🤖';
+const dev = process.env.DEV || '254114018035';
+const DevDreaded = dev.split(",");
+const botname = process.env.BOTNAME || 'DREADED';
+const mode = process.env.MODE || 'public';
+const gcpresence = process.env.GC_PRESENCE || 'false';
+const antionce = process.env.ANTIVIEWONCE || 'true';
+const sessionName = "session";
+const cookies = JSON.parse(process.env.COOKIE || '[{"domain":".youtube.com","expirationDate":1764463277.409877,"hostOnly":false,"httpOnly":false,"name":"__Secure-1PAPISID","path":"/","sameSite":"unspecified","secure":true,"session":false,"storeId":"0","value":"UoBcKfo0_FSAxQ5D/A5ZClpB2xVLQJQGUx","id":1},{"domain":".youtube.com","expirationDate":1764463277.412158,"hostOnly":false,"httpOnly":true,"name":"__Secure-1PSID","path":"/","sameSite":"unspecified","secure":true,"session":false,"storeId":"0","value":"g.a000pghxevPjwTr5Un_D-PS1UxiaEdymANhc_5NWNQgaApthzLU0MOFGGamQ5yqi2vrAqKldbgACgYKASoSARUSFQHGX2MiB0PtUQYJy2_oQLkmMPXgfRoVAUF8yKpuqWya_M2xRHe_6e9o_6TK0076","id":2},{"domain":".youtube.com","expirationDate":1762941611.655441,"hostOnly":false,"httpOnly":true,"name":"__Secure-1PSIDCC","path":"/","sameSite":"unspecified","secure":true,"session":false,"storeId":"0","value":"AKEyXzWtrmvqerXnEweUSkGiFKAn57TBnvoAEBDi6B33Sg4gpMOANgVFwDBU_JtKQXLpisy_","id":3}]');
+const presence = process.env.WA_PRESENCE || 'online';
+const antitag = process.env.ANTITAG || 'true';
+const antidelete = process.env.ANTIDELETE || 'true';
+const autoview = process.env.AUTOVIEW_STATUS || 'true';
+const autolike = process.env.AUTOLIKE_STATUS || 'true';
+const autoread = process.env.AUTOREAD || 'true';
+const autobio = process.env.AUTOBIO || 'false';
+const footer = process.env.FOOTER || "⭐ Powered by: @VOXNET.INC",
 };
 module.exports = config;
