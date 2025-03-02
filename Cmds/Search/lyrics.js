87f7874616e7dbdc09244a58d719807cea2b592d
Vox-Net <voxnet2025@gmail.com>
2025-03-02 12:25:02 +0300

    Update lyrics.js

--- Cmds/Search/lyrics.js
+++ Cmds/Search/lyrics.js
@@ -1,10 +1,10 @@
+const axios = require("axios");
+
 module.exports = async (context) => {
+    const { client, m, text } = context;
 
-const { client, m, text } = context;
-
-const axios = require("axios"); 
     try {
-        const response = await axios.get(`https://api.dreaded.site/api/lyrics?title=${encodeURIComponent(title)}`);
+        const response = await axios.get(`https://api.dreaded.site/api/lyrics?title=${encodeURIComponent(text)}`);
         if (response.data && response.data.lyrics) {
             return response.data.lyrics;
         } else {
