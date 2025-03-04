4424fe95ebba547b6620d9a07ae2d879038ef0b5
Vox-Net <voxnet2025@gmail.com>
2025-03-04 17:10:49 +0300

    Update alive.js

--- Cmds/General/alive.js
+++ Cmds/General/alive.js
@@ -1,10 +1,35 @@
-//alive.js
+const fs = require("fs");
+const path = require("path");
 
 module.exports = async (context) => {
     const { client, m, prefix } = context;
 
-const botname = process.env.BOTNAME || "KANAMBO";
+    const botname = process.env.BOTNAME || "VOX-MD";
 
- await client.sendMessage(m.chat, { image: { url: 'https://avatars.githubusercontent.com/u/106575586?v=4' }, caption: `Hello ${m.pushName}, KANAMBO is active now.\n\nType ${prefix}menu to see my command list..\n\nSome important links concerning the bot are given below.\n\nOfficial website:\n Soon😔\n\nPairing site:\n https://pair.dreaded.site.\n\nRandom APIs site:\nhttps://api.dreaded.site\n\nThis free random APIs are meant for other developers and may not always work.\n\nXd );`, fileLength: "9999999999898989899999999" }, { quoted: m }); 
+    // Get random image from ./Voxmdgall/
+    const galleryPath = path.join(__dirname, "../Voxmdgall");
+    const files = fs.readdirSync(galleryPath).filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));
+    const randomImage = files.length > 0 ? path.join(galleryPath, files[Math.floor(Math.random() * files.length)]) : null;
 
-}
+    // Construct alive message
+    const aliveMessage = `✨ *${botname} is Online!*\n\n` +
+        `👋 Hello *${m.pushName}*, I'm here to assist you.\n\n` +
+        `📌 *Type:* \`${prefix}menu\` *to see my commands.*\n\n` +
+        `⚡ Stay connected, and let's have some fun!\n\n` +
+        `_Powered by VOX-MD_ 🚀`;
+
+    // Send response with random image
+    if (randomImage) {
+        await client.sendMessage(
+            m.chat,
+            {
+                image: { url: `file://${randomImage}` },
+                caption: aliveMessage,
+                fileLength: "9999999999898989899999999"
+            },
+            { quoted: m }
+        );
+    } else {
+        await m.reply(aliveMessage);
+    }
+};
