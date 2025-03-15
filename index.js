eaf64ff21834102d617641a2491c60729989ec38
Vox-Net <voxnet2025@gmail.com>
2025-03-15 15:11:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -205,7 +205,6 @@ client.ev.on("messages.upsert", async (chatUpdate) => {
     });
 
     client.ev.on("creds.update", saveCreds);
-}
 
 app.use(express.static('public'));
 app.get("/", (req, res) => res.sendFile(__dirname + '/index.html'));
