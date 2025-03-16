ca36daebe898b53e716cc62917ee264b35dc0fee
Vox-Net <voxnet2025@gmail.com>
2025-03-16 08:35:45 +0300

    Update neon.js

--- Cmds/Glow Text/neon.js
+++ Cmds/Glow Text/neon.js
@@ -1,4 +1,4 @@
-const { generateTextProImage } = require("../../textpro");
+const  generateTextProImage = require("../../textpro");
 
 module.exports = async (context) => {
     const { client, m, args } = context;
