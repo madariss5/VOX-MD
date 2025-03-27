9b6cc917f0cd713923bb3a3c52e2b9990d0eafdc
Vox-Net <voxnet2025@gmail.com>
2025-03-27 09:01:03 +0300

    🥷VoxNet🥷

--- Cmds/General/ping.js
+++ Cmds/General/ping.js
@@ -1,9 +1,29 @@
-//ping.js
-
 module.exports = async (context) => {
-        const { client, m, dreadedspeed } = context;
+    const { client, m, dreadedspeed } = context;
 
+    // Get device information
+    const battery = await client.fetchBatteryStatus(); // Fetch battery percentage
+    const phoneInfo = m.deviceInfo || {}; // Get device information
+    const { model, manufacturer, os_version, ram, storage } = phoneInfo;
 
-await m.reply(`Pong\n${dreadedspeed.toFixed(4)}ms`)
+    // Construct response message
+    let response = `*📶 Pong!*\n⏱️ *Speed:* ${dreadedspeed.toFixed(4)}ms\n`;
 
-}
+    if (battery) {
+        response += `🔋 *Battery:* ${battery.percentage}%\n`;
+    }
+    if (model) {
+        response += `📱 *Phone Model:* ${manufacturer} ${model}\n`;
+    }
+    if (os_version) {
+        response += `🛠️ *OS Version:* ${os_version}\n`;
+    }
+    if (ram) {
+        response += `💾 *RAM:* ${ram}GB\n`;
+    }
+    if (storage) {
+        response += `📂 *Storage (ROM):* ${storage}GB\n`;
+    }
+
+    await m.reply(response);
+};
